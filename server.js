35fc41a6387044caa4fb0e580199c0d91e3accb6
Sushil Gaire <rememberme936@gmail.com>
2022-10-01 14:05:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 
 //http://localhost:3000/
 app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
+    res.send("<h1>Homee</h1>")
 })
 
 app.listen(process.env.PORT  || SERVER_PORT, () => {
